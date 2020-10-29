5b9feb66d944f607e8ab091be91e3d600b8bb3f2
Jack Chen <redchenjs@live.com>
2020-10-29 23:01:18 +0800

    app: add update manager

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 // app.js
 
-App();
+App({
+  onLaunch() {
+    const updateManager = wx.getUpdateManager();
+
+    updateManager.onCheckForUpdate(function (res) {
+      console.log('hasUpdate:', res.hasUpdate);
+    });
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success(res) {
+          if (res.confirm) {
+            updateManager.applyUpdate();
+          }
+        }
+      });
+    });
+  }
+});
